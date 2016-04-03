a0b256498741d32bdb562bcd106fa3848a824e05
Claudia Green <claudia@claudiag.me.uk>
2016-04-03 18:40:52 +0100

    Moving between frames, can roll with initial logic. No spares or strikes accounted for. Tests currently passing.

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -6,9 +6,24 @@ describe ('Game', function() {
       game = new Game();
     });
 
-  it('starts each frame with 10 pins', function () {
-    expect(game.pins).toEqual(10);
-  });
+    it('game begins at frame 1', function () {
+      expect(game.currentFrame).toEqual(1);
+    });
 
+    it('can move to next frame', function() {
+      game.nextFrame();
+      expect(game.currentFrame).toEqual(2);
+    });
+
+    it('moves to next frame after two rolls (when first roll is not a strike)', function() {
+      game.firstRoll(1);
+      game.firstRoll(1);
+      expect(game.currentFrame).toEqual(2);
+    });
+
+    it("ends the game after 10 frames", function() {
+        game.currentFrame = 10;
+        expect( function() {game.nextFrame(); } ).toThrow(new Error("Game over"));
+    });
 
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,3 +1,45 @@
 var Game = function () {
-  this.rolls = [];
+  this.currentFrame = 1;
+  this.framez = {1: [],
+                2: [],
+                3: [],
+                4: [],
+                5: [],
+                6: [],
+                7: [],
+                8: [],
+                9: [],
+                10: []};
+  this.pins = 10;
+  this.roll = 1;
 };
+
+Game.prototype.nextFrame = function(){
+  if (this.currentFrame === 10) {
+    throw new Error("Game over")
+  }
+  else {
+    this.currentFrame += 1
+  }
+};
+
+Game.prototype.firstRoll = function(pins) {
+    this.framez[this.currentFrame].push(pins);
+   if (this.framez[this.currentFrame].length === 2) {
+     this.pins = 10
+     this.nextFrame();
+   }
+  //  else {
+  //   this.nextRoll();
+  //   this.pins = this.pins - pins;
+  //  }
+  };
+
+  // Game.prototype.nextRoll = function() {
+  //   if (this.roll >= 2) {
+  //     this.nextFrame();
+  //   }
+  //   else {
+  //     return this.roll += 1;
+  //   }
+  // };
