59f180f25ae96d7c307c9f13b9ec8eda752fc9a2
Claudia Green <claudia@claudiag.me.uk>
2016-04-03 15:16:46 +0100

    1st test

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,4 +1,14 @@
 describe ('Game', function() {
 
+  var game
+
+  beforeEach(function(){
+      game = new Game();
+    });
+
+  it('starts each frame with 10 pins', function () {
+    expect(game.pins).toEqual(10);
+  });
+
 
 });
