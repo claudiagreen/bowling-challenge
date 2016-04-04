9fae6a6014d0b880c36345d92d052049f944a0a9
Claudia Green <claudia@claudiag.me.uk>
2016-04-04 08:43:12 +0100

    Some strike logic implemented. Tests currently passing.

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -10,20 +10,46 @@ describe ('Game', function() {
       expect(game.currentFrame).toEqual(1);
     });
 
-    it('can move to next frame', function() {
-      game.nextFrame();
-      expect(game.currentFrame).toEqual(2);
-    });
-
-    it('moves to next frame after two rolls (when first roll is not a strike)', function() {
-      game.firstRoll(1);
-      game.firstRoll(1);
-      expect(game.currentFrame).toEqual(2);
-    });
-
     it("ends the game after 10 frames", function() {
         game.currentFrame = 10;
         expect( function() {game.nextFrame(); } ).toThrow(new Error("Game over"));
     });
+
+    // it('can move to next frame', function() {
+    //   game.nextFrame();
+    //   expect(game.currentFrame).toEqual(2);
+    // });
+
+    it('moves to next frame after two rolls (when first roll is not a strike)', function() {
+      game.rollBall(1);
+      game.rollBall(1);
+      expect(game.currentFrame).toEqual(2);
+    });
+
+    it('can move to the next roll of the frame', function () {
+     game.rollBall(1);
+     game.rollBall(1);
+     expect(game.roll).toEqual(2);
+    });
+
+    it('stores a score in the correct framez hash for each roll', function() {
+     game.rollBall(3);
+     game.rollBall(4);
+     expect(game.framez[1]).toEqual([3, 4]);
+    });
+
+    it('returns total score for a frame', function() {
+      game.rollBall(2);
+      game.rollBall(7);
+      expect(game.totalFrameScore(1)).toEqual(9);
+    });
+
+    // describe ("strike logic", function() {
+      it('moves to next frame after a strike', function() {
+        game.rollBall(10);
+        expect(game.currentFrame).toEqual(2);
+      });
+
+    // };
 
 });
--- src/bowling.js
+++ src/bowling.js
@@ -23,23 +23,31 @@ Game.prototype.nextFrame = function(){
   }
 };
 
-Game.prototype.firstRoll = function(pins) {
+Game.prototype.rollBall = function(pins) {
     this.framez[this.currentFrame].push(pins);
-   if (this.framez[this.currentFrame].length === 2) {
+   if (pins === 10 || this.framez[this.currentFrame].length === 2) {
      this.pins = 10
      this.nextFrame();
    }
-  //  else {
-  //   this.nextRoll();
-  //   this.pins = this.pins - pins;
-  //  }
+   else {
+    this.rollBallAgain();
+    this.pins = this.pins - pins;
+   }
   };
 
-  // Game.prototype.nextRoll = function() {
-  //   if (this.roll >= 2) {
-  //     this.nextFrame();
-  //   }
-  //   else {
-  //     return this.roll += 1;
-  //   }
-  // };
+  Game.prototype.rollBallAgain = function() {
+    if (this.roll >= 2) {
+      this.nextFrame();
+    }
+    else {
+      return this.roll += 1;
+    }
+  };
+
+  Game.prototype.totalFrameScore = function(frame){
+    var total = 0;
+    for (var i = 0; i < this.framez[frame].length; i++) {
+      total += this.framez[frame][i];
+    }
+    return total;
+  };
